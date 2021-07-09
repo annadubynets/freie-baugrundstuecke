e99561e3cd3232a5878cc453075d1705beceb815
Anna Dubynets <arogulina@gmail.com>
2021-07-09 10:33:45 +0300

    add owl carousel

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,45 @@ $(function() {
         $('select').selectpicker();
     }
 })
+
+$('.products .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        768: {
+            items: 2,
+            nav: true
+        },
+        992: {
+            items: 3,
+            nav: true,
+            loop: false
+        }
+    }
+})
+
+$('.partners .owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+            nav: true
+        },
+        768: {
+            items: 3,
+            nav: true
+        },
+        992: {
+            items: 5,
+            nav: true,
+            loop: false
+        }
+    }
+})

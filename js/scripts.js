0c6585fbd6e3eea20fa52325735cfa2be363a2b1
Ivan Dubynets <ivan.dubynets@gmail.com>
2021-07-10 12:38:42 +0300

    basic map integration

--- js/scripts.js
+++ js/scripts.js
@@ -4,50 +4,54 @@ $(function() {
     }
 })
 
-$('.products .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 7000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            nav: true
-        },
-        768: {
-            items: 2,
-            nav: true
-        },
-        992: {
-            items: 3,
-            nav: true,
-            loop: false
+if ($('.products .owl-carousel').length > 0) {
+    $('.products .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            768: {
+                items: 2,
+                nav: true
+            },
+            992: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
         }
-    }
-})
+    })
+}
 
-$('.partners .owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    autoplay: true,
-    autoplayTimeout: 7000,
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 2,
-            nav: true
-        },
-        768: {
-            items: 3,
-            nav: true
-        },
-        992: {
-            items: 5,
-            nav: true,
-            loop: false
+if ($('.partners .owl-carousel').length > 0) {
+    $('.partners .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 7000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true
+            },
+            768: {
+                items: 3,
+                nav: true
+            },
+            992: {
+                items: 5,
+                nav: true,
+                loop: false
+            }
         }
-    }
-})
+    });
+}
